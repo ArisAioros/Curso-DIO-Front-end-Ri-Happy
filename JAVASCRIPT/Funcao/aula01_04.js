1b6b5be7eb8df717a1c91014e740e0a7bec9cc9e
ArisAioros <souaurelice@hotmail.com>
2024-12-19 09:03:33 -0300

    Utima_tima_atualização

---
+++ JAVASCRIPT/Funcao/aula01_04.js
@@ -0,0 +1,16 @@
+function escrevaSeuNome(nome) {
+    console.log('Meu nome é ' + nome);
+}
+
+escrevaSeuNome('Vitor');
+escrevaSeuNome('Renan');
+
+function verificarIdade(idade) {
+    if(idade >= 18) {
+        console.log('Maior');
+        } else {
+            console.log('Menor')
+        }
+}
+
+verificarIdade(11);
